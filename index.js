331f18a9d7a637137028d0dd35ee5b6a447c0ff6
TomasManueltm <antoniomanuelbaptistabaptista@gmail.com>
2022-05-19 00:18:28 +0100

    rejustes css, html e js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var produto = document.querySelectorAll(".produtos ul li"),
-    toggle = document.querySelector(".toggle");
+let i = 1,
+    numb = document.querySelector('.numb'),
+    addCarro = document.querySelector(".input_carro"),
+    toggle = document.querySelector(".toggle"),
+    icrementa = document.querySelector('.inputs_asc'),
+    decrementa = document.querySelector('.inputs_desc'),
+    produto = document.querySelectorAll(".produtos ul li"),
+    preco = document.querySelector('.informacao p span');
+    countProduto = document.querySelector(".count-produto");
 
 
 
+
+
+
+
+
+
+
+// menu toggle
 toggle.addEventListener("click", function(){
     if(document.querySelector(".menu ._ ul").className != "attr"){
         document.querySelector(".menu ._ ul").classList.add("attr");
@@ -11,13 +26,59 @@ toggle.addEventListener("click", function(){
     }
 })
 
-
+// controlers de produtos no catalogo
 produto.forEach((item)=>{
-    item.classList.remove('point');
     item.addEventListener("click", function(e){
         e.preventDefault();
+        clear();
         (e.path[1].className == "point") ? item.classList.remove('point') : item.classList.add('point');
         document.querySelector(".produtoImg img").src = e.target.attributes.src.value;
     })
 })
 
+
+function clear(){
+    produto.forEach((item)=>{
+        item.classList.remove('point');    
+    })
+}
+
+icrementa.addEventListener("click", function(){
+    i++
+    numb.innerHTML = i;
+    // preco.innerHTML = preco.innerText*i;
+})
+
+decrementa.addEventListener("click", function(){
+    (i < 1) ? i=1 : i--;
+    (i >= 1) ? numb.innerHTML = i : '';
+    // (i > 0) ? preco.innerHTML = (preco.innerText*i/2) : '';
+})
+
+addCarro.addEventListener("click", function(){
+    let produtoItens = parseInt(parseInt(numb.innerText)+parseInt(countProduto.innerText));
+    countProduto.innerHTML = produtoItens;
+
+    // salva sessao
+    localStorage.setItem("carro", parseInt(countProduto.innerText));
+})
+
+
+
+
+// sessao banco improvisado
+if(!window.localStorage.getItem("carro"))
+{
+  localStorage.setItem("carro","0");
+  countProduto.innerHTML = localStorage.getItem("carro");
+}
+else
+{
+    countProduto.innerHTML = localStorage.getItem("carro");
+}
+
+
+// repositirio de dados
+let dados = [
+    {id: 1, produto: ""}
+]
